8b2ab278d99e3a0ad8fae479f938dcf140dd250d
estefanabermeo <131408276+estefanabermeo@users.noreply.github.com>
2023-06-05 20:00:44 -0600

    Update evidenciafinalu6_a01367558.py

--- evidenciafinalu6_a01367558.py
+++ evidenciafinalu6_a01367558.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/11220-Xhs2CRdxLS8jq05e9Ri1OLGNnND/view?usp=sharing")
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/1yYtFrkbm7uwifDIutLDdgCrUzTjj5WNJ/edit?usp=sharing&ouid=106792942433502689439&rtpof=true&sd=true")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
