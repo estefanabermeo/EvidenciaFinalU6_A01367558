5b4bdf23c26c432478d2612042cde7bce8ad3dab
estefanabermeo <131408276+estefanabermeo@users.noreply.github.com>
2023-06-05 19:26:47 -0600

    Update evidenciafinalu6_a01367558.py

--- evidenciafinalu6_a01367558.py
+++ evidenciafinalu6_a01367558.py
@@ -17,6 +17,7 @@ df = pd.read_csv("https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa=pd.DataFrame()
-mapa=mapa.dropna()
-st.map(mapa.astype(int))
+mapa = pd.DataFrame()
+mapa['Latitude'] = df['Latitude']
+mapa['Longitude'] = df['Longitude']
+mapa = mapa.dropna()
